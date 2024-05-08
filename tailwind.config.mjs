04719acef3ac96281366b2b6974c0cf77d98881c
josvillegas <josvillegas97@gmail.com>
2024-05-08 09:04:02 -0500

    footer and deploy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "2xlsm": ".5px .5px 10px 0.9px rgba(0,0,0, 0.3)",
+      },
+    },
+  },
+  plugins: [],
+};
