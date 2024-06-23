01e3c3c85980d6813bd2bc28792da8d09a1c3111
josvillegas <josvillegas97@gmail.com>
2024-06-23 15:05:36 -0500

    remove configuration githubpages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      boxShadow: {
-        "2xlsm": ".5px .5px 10px 0.9px rgba(0,0,0, 0.3)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
